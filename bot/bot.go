83129f30257d61457f4a482fa399e6836570ed61
outlawprecision <130953979+outlawprecision@users.noreply.github.com>
2023-04-16 23:47:14 -0500

    Add files via upload

---
+++ bot/bot.go
@@ -0,0 +1,74 @@
+package bot
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+	"https://github.com/outlawprecision/RobinHoodDKP-BOT/db"
+	"https://github.com/outlawprecision/RobinHoodDKP-BOT/utils"
+)
+
+type Bot struct {
+	Session *discordgo.Session
+	DB      *db.DB
+}
+
+func NewBot(session *discordgo.Session, db *db.DB) *Bot {
+	return &Bot{
+		Session: session,
+		DB:      db,
+	}
+}
+
+func (b *Bot) Start() error {
+	b.Session.AddHandler(b.onMessageCreate)
+
+	err := b.Session.Open()
+	if err != nil {
+		return fmt.Errorf("error opening connection to Discord: %v", err)
+	}
+	return nil
+}
+
+func (b *Bot) onMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if !strings.HasPrefix(m.Content, "!") {
+		return
+	}
+
+	args := strings.Fields(m.Content)
+	command := strings.ToLower(args[0][1:])
+
+	switch command {
+	case "checkbalance":
+		b.checkBalance(s, m)
+	case "addpoints":
+		b.addPoints(s, m, args)
+	case "removepoints":
+		b.removePoints(s, m, args)
+	case "watchevent":
+		b.watchEvent(s, m, args)
+	default:
+		s.ChannelMessageSend(m.ChannelID, "Unknown command.")
+	}
+}
+
+func (b *Bot) checkBalance(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Implement check balance logic
+}
+
+func (b *Bot) addPoints(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
+	// Implement add points logic
+}
+
+func (b *Bot) removePoints(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
+	// Implement remove points logic
+}
+
+func (b *Bot) watchEvent(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
+	// Implement watch event logic
+}
